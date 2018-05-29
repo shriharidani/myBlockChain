829d4f2f9a72b445733d336a7b36fd53cfdcf1f4
shrihari dani <shriharidani@shriharis-MacBook-Air.local>
2018-05-29 22:54:46 +0530

    Transaction Pool

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -2,10 +2,16 @@ const Websocket = require('ws');
 const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
+const MESSAGE_TYPES = {
+	chain: 'CHAIN',
+	transaction: 'TRANSACTION'
+};
+
 class P2pServer {
-	constructor(blockchain){
+	constructor(blockchain, transactionPool){
 		this.blockchain = blockchain;
 		this.sockets = [];
+		this.transactionPool = transactionPool;
 	}
 
 	listen(){
@@ -22,7 +28,8 @@ class P2pServer {
 	}
 
 	sendChain(socket){
-		socket.send(JSON.stringify(this.blockchain.chain));
+		socket.send(JSON.stringify({type: MESSAGE_TYPES.chain,chain:
+			this.blockchain.chain}));
 	}
 
 	connectToPeers(){
@@ -35,7 +42,14 @@ class P2pServer {
 	messageHandler(socket) {
 		socket.on('message', message => {
 			const data = JSON.parse(message);
-			this.blockchain.replaceChain(data);
+			switch(data.type) {
+				case MESSAGE_TYPES.chain:
+					this.blockchain.replaceChain(data);
+					break;
+				case MESSAGE_TYPES.transaction:
+					this.transactionPool.updateOrAddTransaction(data.transaction);
+					break;
+			}
 		});
 	}
 
@@ -43,6 +57,14 @@ class P2pServer {
 		this.sockets.forEach(socket => {
 			this.sockets.forEach(socket => this.sendChain(socket));
 		});
+	}
+
+	sendTransaction(socket, transaction) {
+		socket.send(JSON.stringify({type: MESSAGE_TYPES.transaction, transaction}));
+	}
+
+	broadcastTransaction(transaction) {
+		this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
 	}
 }
 
